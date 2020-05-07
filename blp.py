a329f680913cbaac1156cdcf46c5fcda6d34e3bb
Steven Zhang <steven.zhang@CLAMC.COM.HK>
2020-05-07 14:53:49 +0800

    add readBlpFile() function, returns both date and CLO, nonCLO positions

--- blp.py
+++ blp.py
@@ -7,13 +7,105 @@ from itertools import takewhile, dropwhile, filterfalse, chain
 from functools import partial, reduce
 from toolz.functoolz import compose
 from toolz.itertoolz import groupby as groupbyToolz
-from utils.iter import pop
+from utils.iter import pop, firstOf
 from utils.excel import worksheetToLines
+from utils.utility import mergeDict
 from risk_report.utility import getCurrentDirectory
 from xlrd import open_workbook
+from datetime import datetime
 from os.path import join
-import logging
+import logging, re
 logger = logging.getLogger(__name__)
+
+
+
+def lognContinue(msg, x):
+	logger.debug(msg)
+	return x
+
+
+def lognRaise(msg):
+	logger.error(msg)
+	raise ValueError
+
+
+
+def readBlpFile(file):
+	"""
+	[String] file => ( [String] date (yyyy-mm-dd)
+					 , [Iterable] CLO positions consolidated
+				     , [Iterable] non-CLO positions consolidated)
+					 )
+
+	The function to expose to other modules. Tt gets the long positions from 
+	Bloomberg Excel, take out those that do not need liquidity measure (cash etc.), 
+	split into CLO positions and non-CLO positions, the condolidate positions
+	across all portofolios.
+	"""
+
+	# [Iterable] lines => [List] line that contains the date
+	findDateLine = partial(
+		firstOf
+	  , lambda line: len(line) > 0 and line[0].lower().startswith('mav: mav')
+	)
+
+
+	# [String] header containing date => [String] date (yyyy-mm-dd)
+	# The header looks like: MAV: MAV (12/09/2019 01:17:01)
+	getDateFromString = compose(
+		lambda s: datetime.strftime(datetime.strptime(s, '%m/%d/%Y'), '%Y-%m-%d')
+	  , lambda s: s[1:].split()[0]
+	  , lambda m: lognRaise('Failed to get date from line') if m == None else m.group(1)
+	  , lambda s: re.match('.*(\(.*\))', s)
+	)
+
+
+	getDateFromLines = compose(
+		getDateFromString
+	  , lambda line: lognRaise('Failed to find date line') if line == None else line[0]
+	  , findDateLine
+	)
+
+
+	updatePositionWithDate = lambda date, p: \
+		mergeDict(p, {'Date': date})
+
+
+	isCLOPortfolio = lambda p: p['Account Code'] in \
+						['12229', '12734', '12366', '12630', '12549', '12550', '13007']
+
+
+	"""
+	[Iterable] positions => ( [Iterable] consolidated CLO positions
+							, [Iterable] consolidated non CLO positions
+							)
+
+	Split the positions into CLO and non-CLO group, then consolidate them
+	"""
+	splitCLO = lambda positions: \
+		reduce( lambda acc, el: (chain(acc[0], [el]), acc[1]) if isCLOPortfolio(el) else \
+								(acc[0], chain(acc[1], [el])) 
+	  		  , positions
+	  		  , ([], [])
+	  		  )
+
+
+	updateSplitConsolidate = compose(
+	  	lambda t: ( consolidate(t[0])
+				  , consolidate(t[1])
+				  )
+	  , splitCLO
+	  , lambda date, positions: map(partial(updatePositionWithDate, date), positions)
+	)
+
+
+	return \
+	compose(
+		lambda t: (t[0], *updateSplitConsolidate(t[0], t[1]))
+	  , lambda lines: (getDateFromLines(lines), getLongHoldings(lines))
+	  , fileToLines
+	  , lambda file: lognContinue('readBlpFile(): {0}'.format(file), file)
+	)(file)
 
 
 
@@ -29,29 +121,27 @@ def fileToLines(file):
 
 
 """
-	[String] file => [Iterable] positions
+	[Iterable] lines => [Iterable] positions
 
 	Read all positions from the Bloomberg Excel file.
 """
 getPositions = compose(
 	  lambda t: map(lambda line: dict(zip(t[0], line)), t[1])
 	, lambda lines: (pop(lines), lines)
-	, partial(dropwhile, lambda line: line[0] != 'Name')
-	, fileToLines
+	, lambda lines: dropwhile(lambda line: line[0] != 'Name', lines)
 )
 
 
 
-isEmptyPosition = lambda p: p['Position'] == ''
-isShortPosition = lambda p: p['Position'] <= 0
+emptyorShortPosition = lambda p: p['Position'] == '' or p['Position'] <= 0
 unwantedPositionType = lambda p: p['Asset Type'] in \
 	['Cash', 'Foreign Exchange Forward', 'Repo Liability', 'Money Market']
-isDIFPosition = lambda p: p['Account Code'][:5] == '19437'
+difPosition = lambda p: p['Account Code'][:5] == '19437'
 
 
 
 """
-	[String] file => [Iterable] long positions without cash and DIF
+	[Iterable] lines => [Iterable] long positions without cash and DIF
 	
 	Read positions, but exclude:
 
@@ -60,72 +150,36 @@ isDIFPosition = lambda p: p['Account Code'][:5] == '19437'
 	3) DIF positions (we will take DIF from Geneva)
 """
 getLongHoldings = compose(
-	partial(filterfalse, isDIFPosition)
+	partial(filterfalse, difPosition)
   , partial(filterfalse, unwantedPositionType)
-  , partial(filterfalse, isShortPosition)
-  , partial(filterfalse, isEmptyPosition)
+  , partial(filterfalse, emptyorShortPosition)
   , getPositions
 )
 
 
 
-# 12298 has only one equity holding and cash, no bo
-isCLOPortfolio = lambda p: p['Account Code'] in \
-					['12229', '12734', '12366', '12630', '12549', '12550', '13007']
-
-
-
 """
-	(name, list of records under the name) => [Dictioanry] consolidated record
+	[List] group (positions of the same security) 
+		=> [Dictioanry] consolidated record
 
 	Example:
 
-	( '1 HK'
-	, [ {'Name': '1 HK', 'Position': 100, ...}
+	[ {'Name': '1 HK', 'Position': 100, ...}
 	  , {'Name': '1 HK', 'Position': 200, ...}
-	  ]
-	)
+	]
 
 	=>
 
 	{'Name': '1 HK', 'Position': 300, ...}
 """
-consolidateGroup = lambda t: \
-	{ 'Name': t[0]
-	, 'ISIN': t[1][0]['ISIN']
-	, 'Asset Type': t[1][0]['Asset Type']
-	, 'Currency': t[1][0]['Currency']
-	, 'Position': sum(map(lambda x: x['Position'], t[1]))
-	}
+consolidateGroup = lambda group: \
+	mergeDict(group[0].copy(), {'Position': sum(map(lambda p: p['Position'], group))})
 
 
 
 # [Iterable] positions => [Iterable] consolidated positions
 consolidate = compose(
 	  partial(map, consolidateGroup)
-	, lambda d: d.items()
+	, lambda d: d.values()
 	, partial(groupbyToolz, lambda p: p['Name'])
-)
-
-
-
-"""
-	[String] file => ( [Iterable] CLO positions consolidated
-					 , [Iterable] non-CLO positions consolidated)
-
-	This is the function exposed to others, it gets the long positions from 
-	Bloomberg Excel, take out those that do not need liquidity measure (cash etc.), 
-	split into CLO positions and non-CLO positions, the condolidate positions
-	across all portofolios.
-
-"""
-getConsolidatedHoldings = compose(
-	lambda t: (consolidate(t[0]), consolidate(t[1]))
-  , lambda positions: \
-		reduce( lambda acc, el: (chain(acc[0], [el]), acc[1]) if isCLOPortfolio(el) \
-  		   							else (acc[0], chain(acc[1], [el])) 
-  		   	  , positions
-  		      , ([], [])
-  		   	  )
-  , getLongHoldings
 )
--- test/test_ALL.py
+++ test/test_ALL.py
@@ -2,7 +2,7 @@
 # 
 
 import unittest2
-from risk_report.blp import getLongHoldings, getConsolidatedHoldings
+from risk_report.blp import readBlpFile, getLongHoldings, fileToLines
 from risk_report.utility import getCurrentDirectory
 from functools import partial
 from utils.iter import firstOf
@@ -24,14 +24,16 @@ class TestALL(unittest2.TestCase):
 
 	def testGetLongHoldings(self):
 		inputFile = join(getCurrentDirectory(), 'samples', 'risk_m2_sample.xls')
-		holdings = list(getLongHoldings(inputFile))
+		holdings = list(getLongHoldings(fileToLines(inputFile)))
 		self.assertEqual(25, len(holdings))
 
 
 
 	def testGetConsolidatedHoldings(self):
 		inputFile = join(getCurrentDirectory(), 'samples', 'risk_m2_sample.xls')
-		clo, nonCLO = getConsolidatedHoldings(inputFile)
+		date, clo, nonCLO = readBlpFile(inputFile)
+		self.assertEqual('2019-12-09', date)
+		
 		clo, nonCLO = list(clo), list(nonCLO)
 		self.assertEqual(2, len(clo))
 		self.assertEqual(12, len(nonCLO))
@@ -45,6 +47,7 @@ class TestALL(unittest2.TestCase):
 		self.assertEqual('Corporate Bond', p['Asset Type'])
 		self.assertEqual(15846, p['Position'])
 		self.assertEqual('USD', p['Currency'])
+		self.assertEqual('2019-12-09', p['Date'])
 
 
 
@@ -53,3 +56,4 @@ class TestALL(unittest2.TestCase):
 		self.assertEqual('Corporate Bond', p['Asset Type'])
 		self.assertEqual(4000, p['Position'])
 		self.assertEqual('USD', p['Currency'])
+		self.assertEqual('2019-12-09', p['Date'])
