f8060ff24300b249c5cdd381b1a2e96bd65a6f54
Steven Zhang <steven.shutao@gmail.com>
2020-05-24 23:03:47 +0800

    doing now

--- asset.py
+++ asset.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 #
-# Asset allocation logic
+# Asset allocation logic for SFC
 # 
 
 import logging
@@ -8,15 +8,15 @@ logger = logging.getLogger(__name__)
 
 
 
-def sfcCountry(investment):
+def country(investment):
 	"""
 	The logic is:
 
-	1) For cash equivalents, leave it, no country is assigned.
-	2) For equity asset class, use Bloomberg "CNTRY_ISSUE_ISO" field
-	3) For other asset class, use Bloomberg "CNTRY_OF_RISK" field
+	1) For equity asset class, use Bloomberg "CNTRY_ISSUE_ISO" field
+	2) For other asset class other than cash, use Bloomberg "CNTRY_OF_RISK" field
+	3) For cash, it will throw an exception because country does not make sense
+		in this case.
 	"""
-
 	return 0
 
 
@@ -25,8 +25,42 @@ def assetClass(investment):
 	"""
 	The logic is:
 
-	
+	If it's cash on hand, payables and receivables, money market instrucments 
+	(say fixed deposit), asset class = "Cash Equivalents"
+
+	If it's not cash, then use LQA "MARKET_SECTOR_DES" field to lookup:
+
+	If LQA market sector = "Equity", then asset class = "Equity", sub category
+	"Listed Equity" if Bloomberg field "EXCH_MARKET_STATUS" = "ACTV", else
+	sub category "Unlisted Equity".
+
+	If it's not equity and its Bloomberg field "CAPITAL_CONTINGENT_SECURITY" 
+	= "Y", then asset class is "Additional Tier 1, Contingent Convertibles 
+	and similar instrucments"
+
+	Else use the below mapping
+
+	Corp -> Fixed Income, sub catetory "Corporate Bond"
+	Govt -> Fixed Income, sub catetory "Government Bond"
+	Comdty -> Derivatives, sub category "Derivatives"
 	"""
+	return 0
+
+
+
+def isFinancial(investment):
+	"""
+	The logic:
+
+	1) If asset class is cash equivalent, then throw exception because it does
+		not make sense.
+	2) Otherwise use Bloomberg "INDUSTRY_SECTOR", if return result is "Financial",
+		then YES, othereise NO.
+
+	What about derivatives? Cannot see the logic in Excel
+	"""
+	return 0
+
 
 
 
